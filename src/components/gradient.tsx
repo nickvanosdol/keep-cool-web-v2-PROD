333e5a792bafcfe605ce410ad7150ca781a94f4a
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2025-02-18 15:03:25 -0700

    Update gradient.tsx

--- src/components/gradient.tsx
+++ src/components/gradient.tsx
@@ -9,7 +9,7 @@ export function Gradient({
       {...props}
       className={clsx(
         className,
-        'bg-linear-115 from-[#7FF3F9] from-28% via-[#7F9AF9] via-70% to-[#5C14D8] sm:bg-linear-145',
+        'bg-linear-115 from-[#7FF3F9] from-13% via-[#7F9AF9] via-60% to-[#5C14D8] sm:bg-linear-145',
       )}
     />
   )
@@ -21,7 +21,7 @@ export function GradientBackground() {
       <div
         className={clsx(
           'absolute -top-44 -right-60 h-60 w-[36rem] transform-gpu md:right-0',
-          'bg-linear-115 from-[#7FF3F9] from-28% via-[#7F9AF9] via-70% to-[#5C14D8]',
+          'bg-linear-115 from-[#7FF3F9] from-13% via-[#7F9AF9] via-60% to-[#5C14D8]',
           'rotate-[-10deg] rounded-full blur-3xl',
         )}
       />
