ea4b90c910a0e37f835c3f475dc7ea33e19b3573
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2025-02-26 13:40:46 -0700

    Update queries.ts

--- beehiiv/queries.ts
+++ beehiiv/queries.ts
@@ -26,7 +26,7 @@ export async function getSubscriberCount(
 
 export async function getFeaturedPosts(fallbackPosts: PostType[]) {
   const params = new URLSearchParams({
-    order_by: 'publish_date',
+    order_by: 'created',
     direction: 'desc',
     limit: '10',
     hidden_from_feed: 'false',
